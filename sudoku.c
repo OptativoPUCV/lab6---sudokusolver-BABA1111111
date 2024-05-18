6727cc9f4796a8197c0aa9df5656299139853924
Your Name <you@example.com>
2024-05-18 23:13:30 +0000

    new try

--- sudoku.c
+++ sudoku.c
@@ -57,9 +57,9 @@ int is_valid(Node* n){
       }
    }
    
-   for (int i = 0; i < 9; i++){
+   for (int j = 0; j < 9; j++){
       int col[10] = {0};
-      for (int j = 0; j < 9; j++){
+      for (int i = 0; i < 9; i++){
          int num = n->sudo[i][j];
          if (num != 0){
             if (col[num] == 1){
