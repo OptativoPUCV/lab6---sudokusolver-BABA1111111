e9910c54caa8ab9528a7c0918c2fdce0bf5b8aef
Your Name <you@example.com>
2024-05-18 23:45:18 +0000

    new try

--- sudoku.c
+++ sudoku.c
@@ -138,6 +138,19 @@ int is_final(Node* n){
 }
 
 Node* DFS(Node* initial, int* cont){
+   // Cree un stack S (pila) e inserte el nodo.
+   // Mientras el stack S no se encuentre vacío:
+   // a) Saque y elimine el primer nodo de S.
+   // b) Verifique si corresponde a un estado final, si es así retorne el nodo.
+   // c) Obtenga la lista de nodos adyacentes al nodo.
+   // d) Agregue los nodos de la lista (uno por uno) al stack S.
+   // e) Libere la memoria usada por el nodo.
+   // Si terminó de recorre el grafo sin encontrar una solución, retorne NULL.
+   // Almacene en la variable cont, la cantidad de iteraciones que realiza el algoritmo.
+   // Puede ver un código de ejemplo en las diapos.
+   // Recuerde revisar las operaciones del TDA Stack en el archivo list.h.
+
+   
   return NULL;
 }
 
