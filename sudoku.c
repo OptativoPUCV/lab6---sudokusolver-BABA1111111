4d11be97563eb211b4cbfd152b545f7c57a33aaf
Your Name <you@example.com>
2024-05-18 22:33:38 +0000

    new try

--- sudoku.c
+++ sudoku.c
@@ -54,8 +54,8 @@ List* get_adj_nodes(Node* n){
 
    int fil = -1, col = -1;
    
-   for (int i = 0; i < 9 && fil == 0; i++){
-      for (int j = 0; i < 9 && col == 0; i++){
+   for (int i = 0; i < 9 && fil == -1; i++){
+      for (int j = 0; i < 9 && col == -1; i++){
          if (n->sudo[i][j] == 0){
             fil = i;
             col = j;
